76c9c41b1e24964497574aac0dd2f16c4e159193
yakovsmonarch <medvejonok07@gmail.com>
2020-11-12 20:37:44 +0800

    Create repository

---
+++ DoorAlarm.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAlarm : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+
+    private bool _inHouse;
+    private float _directionIncreasingVolume = 0.01f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<WeapointMovement>(out WeapointMovement movement))
+        {
+            if(_inHouse == false)
+            {
+                _audioSource.Play();
+                _inHouse = true;
+            }
+            else
+            {
+                _audioSource.Stop();
+                _inHouse = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(_audioSource.volume == 1)
+        {
+            _directionIncreasingVolume *= -1;
+        }
+        if(_audioSource.volume == 0)
+        {
+            _directionIncreasingVolume *= -1;
+        }
+        _audioSource.volume += _directionIncreasingVolume;
+
+    }
+}
---
+++ WeapointMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeapointMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _path;
+
+    private Transform[] _points;
+    private int _currentPoint;
+
+    private void Start()
+    {
+        _points = new Transform[_path.childCount];
+        for(int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+
+    }
+
+    private void Update()
+    {
+        Transform target = _points[_currentPoint];
+        transform.position = Vector2.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+
+        if(transform.position == target.position)
+        {
+            _currentPoint++;
+            if(_currentPoint >= _points.Length)
+            {
+                _currentPoint = 0;
+            }
+        }
+    }
+}
